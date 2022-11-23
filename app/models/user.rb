8594c85e7ff228b2003d21172f8896383950d3ae
lbackman <lucasbackman@gmail.com>
2022-11-23 11:22:04 +0200

    Add validations on User model
    Ensures that the username is present, unique and between 4 and 20 chars.
    
    Validates given email against a regexp:
    https://stackoverflow.com/questions/4770133/rails-regex-for-email-validation
    
    Ensures that the bio is no more than 500 chars.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  validates :username,
+    presence: true,
+    length: { in: 4..20 },
+    uniqueness: {
+      message: -> (_object, data) do
+        "#{data[:value]} is already taken!"
+      end
+    }
+
+  validates :email,
+    presence: true,
+    format: {
+      with: /\A(|(([A-Za-z0-9]+_+)|(\+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/
+    }
+
+  validates :bio, length: { maximum: 500 }
 end
