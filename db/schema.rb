e6564d885ed54a79600a8df66c473527bffc13ae
lbackman <lucasbackman@gmail.com>
2022-11-23 11:45:53 +0200

    Add user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_092733) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_093835) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_092733) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
