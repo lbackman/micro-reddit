bc7f3ac3d5ccb07cf17398c2d112a0cbbc443efe
lbackman <lucasbackman@gmail.com>
2022-11-23 11:28:52 +0200

    Create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_084503) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_092733) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
