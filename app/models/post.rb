dd3257f3bf8f3005427da93585c63499b0a558b5
lbackman <lucasbackman@gmail.com>
2022-11-23 11:36:10 +0200

    Add validations for Post model
    Ensure that title and post body are present and don't exceed their
    limits.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  validates :title,
+    presence: true,
+    length: { maximum: 180 }
+  
+  validates :content,
+    presence: true,
+    length: { maximum: 5000 }
 end
